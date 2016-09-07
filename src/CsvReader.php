390b27838e2baaeda3181d8675ed916ce4849891
Marc Ermshaus <marc@ermshaus.org>
2016-09-07 15:46:55 +0200

    Add CsvReader.

---
+++ src/CsvReader.php
@@ -0,0 +1,199 @@
+<?php
+
+/*
+ * This file is part of the kaloa/filesystem package.
+ *
+ * For full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Kaloa\Filesystem;
+
+use Exception;
+use LogicException;
+
+/**
+ *
+ */
+final class CsvReader
+{
+    /**
+     * @var resource
+     */
+    private $stream;
+
+    /**
+     * @var string
+     */
+    private $delimiter;
+
+    /**
+     * @var string
+     */
+    private $enclosure;
+
+    /**
+     * @var string
+     */
+    private $escape;
+
+    /**
+     * @var string
+     */
+    private $encoding;
+
+    /**
+     * @var int
+     */
+    private $fetchRowLineCounter = 0;
+
+    /**
+     * @var array
+     */
+    private $fetchRowKeys = array();
+
+    /**
+     * @param resource $stream
+     * @param string $encoding
+     * @param string $delimiter
+     * @param string $enclosure
+     * @param string $escape
+     * @throws Exception
+     */
+    public function __construct($stream, $encoding = 'UTF-8', $delimiter = ',', $enclosure = '"', $escape = '\\')
+    {
+        $this->stream = $stream;
+
+        $this->assertStream();
+
+        $this->delimiter = $delimiter;
+        $this->enclosure = $enclosure;
+        $this->escape    = $escape;
+        $this->encoding  = $encoding;
+    }
+
+    /**
+     * @return array
+     */
+    public function fetchAll()
+    {
+        $data = array();
+
+        while ($row = $this->getNextRow()) {
+            $data[] = $this->negotiateEncoding($row);
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $keys If not empty, use as array keys. Otherwise read keys
+     *                    from first line of input data
+     * @return array
+     * @throws Exception
+     */
+    public function fetchAllAssoc(array $keys = array())
+    {
+        $headerFlag = true;
+        $keysCount = count($keys);
+
+        if ($keysCount > 0) {
+            $headerFlag = false;
+        }
+
+        $data = array();
+
+        while ($row = $this->getNextRow()) {
+            if ($headerFlag) {
+                $headerFlag = false;
+                $keys = $this->negotiateEncoding($row);
+                $keysCount = count($keys);
+                continue;
+            }
+
+            if (count($row) !== $keysCount) {
+                throw new Exception('Malformed line in CSV input');
+            }
+
+            $data[] = array_combine($keys, $this->negotiateEncoding($row));
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $keys
+     * @return array|bool
+     */
+    public function fetchAssoc(array $keys = array())
+    {
+        $headerFlag = empty($keys);
+
+        $row = $this->getNextRow();
+
+        if (!is_array($row)) {
+            // EOF
+            return false;
+        }
+
+        if (0 === $this->fetchRowLineCounter) {
+            if ($headerFlag) {
+                $this->fetchRowKeys = $this->negotiateEncoding($row);
+                $this->fetchRowLineCounter++;
+                return $this->{__FUNCTION__}($keys);
+            } else {
+                $this->fetchRowKeys = $keys;
+            }
+        }
+
+        $this->fetchRowLineCounter++;
+
+        return array_combine($this->fetchRowKeys, $this->negotiateEncoding($row));
+    }
+
+    /**
+     * @return array|bool
+     */
+    private function getNextRow()
+    {
+        $this->assertStream();
+
+        $tmp = fgetcsv($this->stream, 0, $this->delimiter, $this->enclosure, $this->escape);
+
+        if ($tmp !== false && !is_array($tmp)) {
+            throw new LogicException('Unexpected error in fgetcsv call');
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function assertStream()
+    {
+        set_error_handler(function () {}, E_WARNING);
+        $tmp = get_resource_type($this->stream);
+        restore_error_handler();
+
+        if (!is_string($tmp) || 'stream' !== $tmp) {
+            throw new Exception('Supplied input is not a valid stream resource');
+        }
+    }
+
+    /**
+     * @param array $elements
+     * @return array
+     */
+    private function negotiateEncoding(array $elements)
+    {
+        if ('UTF-8' !== $this->encoding) {
+            foreach ($elements as &$element) {
+                $element = mb_convert_encoding($element, 'UTF-8', $this->encoding);
+            }
+            unset($element);
+        }
+
+        return $elements;
+    }
+}
---
+++ tests/Kaloa/Tests/Filesystem/CsvReaderTest.php
@@ -0,0 +1,248 @@
+<?php
+
+/*
+ * This file is part of the kaloa/filesystem package.
+ *
+ * For full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Kaloa\Tests\Filesystem;
+
+use Kaloa\Filesystem\CsvReader;
+use PHPUnit_Framework_TestCase;
+
+/**
+ *
+ */
+class CsvReaderTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testCanRead()
+    {
+        $stream = fopen(__DIR__ . '/csv-files/simple.csv', 'rb');
+        $reader = new CsvReader($stream);
+        $data = $reader->fetchAllAssoc();
+        fclose($stream);
+
+        $expected = array();
+        $expected[] = array('Col a' => 'value 1a', 'Col b' => 'value 1b');
+        $expected[] = array('Col a' => 'value 2a', 'Col b' => 'value 2b');
+
+        $this->assertSame($expected, $data);
+    }
+
+    /**
+     *
+     */
+    public function testCanReadWithCustomDelimiters()
+    {
+        $stream = fopen(__DIR__ . '/csv-files/custom-delimiters.csv', 'rb');
+        $reader = new CsvReader($stream, 'UTF-8', ':', '|');
+        $data = $reader->fetchAllAssoc();
+        fclose($stream);
+
+        $expected = array();
+        $expected[] = array('Col a' => 'value 1a', 'Col b' => 'value 1b');
+        $expected[] = array('Col a' => 'value 2a', 'Col b' => 'value 2b');
+
+        $this->assertSame($expected, $data);
+    }
+
+    /**
+     *
+     */
+    public function testCanReadWithOnlyHeaders()
+    {
+        $csvData = <<<'CSV'
+foo,bar,"foo bar"
+CSV;
+
+        $dataUri = $this->convertToDataUri($csvData);
+
+        $stream = fopen($dataUri, 'rb');
+
+        $reader = new CsvReader($stream);
+
+        $data = $reader->fetchAllAssoc();
+
+        fclose($stream);
+
+        $expected = array();
+
+        $this->assertSame($expected, $data);
+
+    }
+
+    /**
+     *
+     */
+    public function testCanReadWithoutHeaders()
+    {
+        $stream = fopen(__DIR__ . '/csv-files/no-headers.csv', 'rb');
+        $reader = new CsvReader($stream);
+        $data = $reader->fetchAll();
+        fclose($stream);
+
+        $expected = array();
+        $expected[] = array('value 1a', 'value 1b');
+        $expected[] = array('value 2a', 'value 2b');
+
+        $this->assertSame($expected, $data);
+    }
+
+    /**
+     *
+     */
+    public function testCanReadInStreamingMode()
+    {
+        $stream = fopen(__DIR__ . '/csv-files/custom-delimiters.csv', 'rb');
+        $reader = new CsvReader($stream, 'UTF-8', ':', '|');
+        $data = array();
+
+        while ($row = $reader->fetchAssoc()) {
+            $data[] = $row;
+        }
+
+        fclose($stream);
+
+        $expected = array();
+        $expected[] = array('Col a' => 'value 1a', 'Col b' => 'value 1b');
+        $expected[] = array('Col a' => 'value 2a', 'Col b' => 'value 2b');
+
+        $this->assertSame($expected, $data);
+    }
+
+    /**
+     *
+     */
+    public function testCanReadInStreamingModeWithCustomKeys()
+    {
+        $stream = fopen(__DIR__ . '/csv-files/no-headers.csv', 'rb');
+        $reader = new CsvReader($stream);
+
+        $data = array();
+
+        while ($row = $reader->fetchAssoc(array('Col a', 'Col b'))) {
+            $data[] = $row;
+        }
+
+        fclose($stream);
+
+        $expected = array();
+        $expected[] = array('Col a' => 'value 1a', 'Col b' => 'value 1b');
+        $expected[] = array('Col a' => 'value 2a', 'Col b' => 'value 2b');
+
+        $this->assertSame($expected, $data);
+    }
+
+    /**
+     *
+     */
+    public function testCanReadDataStreamInStreamingMode()
+    {
+        $csvData = <<<'CSV'
+|Col a|:|Col b|
+|value 1a|:|value 1b|
+|value 2a|:|value 2b|
+CSV;
+
+        $dataUri = $this->convertToDataUri($csvData);
+
+        $stream = fopen($dataUri, 'rb');
+
+        $reader = new CsvReader($stream, 'UTF-8', ':', '|');
+
+        $data = array();
+
+        while ($row = $reader->fetchAssoc()) {
+            $data[] = $row;
+        }
+
+        fclose($stream);
+
+        $expected = array();
+        $expected[] = array('Col a' => 'value 1a', 'Col b' => 'value 1b');
+        $expected[] = array('Col a' => 'value 2a', 'Col b' => 'value 2b');
+
+        $this->assertSame($expected, $data);
+    }
+
+    /**
+     *
+     */
+    public function testThrowsExceptionOnInvalidStream()
+    {
+        $this->setExpectedException('Exception');
+
+        new CsvReader('invalid');
+    }
+
+    /**
+     *
+     */
+    public function testCanConvertCharset()
+    {
+        $csvData = <<<CSV
+"Thomas Müller","FC Bayern München",Sturm
+"Julian Weigl","Borussia Dortmund",Mittelfeld
+"Serge Gnabry","Werder Bremen",Sturm
+"Gianluigi Buffon",Juventus,Tor
+"Shkodran Mustafi",Arsenal,Abwehr
+"Mario Gómez","VfL Wolfsburg",Sturm
+CSV;
+
+        $csvData = mb_convert_encoding($csvData, 'ISO-8859-1', 'UTF-8');
+
+        $this->assertSame(true, strpos($csvData, "M\xFCller") >= 0);
+        $this->assertSame(true, strpos($csvData, "M\xFCnchen") >= 0);
+
+        $dataUri = $this->convertToDataUri($csvData);
+
+        $stream = fopen($dataUri, 'rb');
+
+        $reader = new CsvReader($stream, 'ISO-8859-1', ',', '"', '\\');
+
+        $data = array();
+
+        while ($row = $reader->fetchAssoc(array('name', 'team', 'position'))) {
+            $data[] = $row;
+        }
+
+        fclose($stream);
+
+        $expected = array('name' => 'Thomas Müller', 'team' => 'FC Bayern München', 'position' => 'Sturm');
+        $this->assertSame($expected, $data[0]);
+
+        $expected = array('name' => 'Mario Gómez', 'team' => 'VfL Wolfsburg', 'position' => 'Sturm');
+        $this->assertSame($expected, $data[5]);
+    }
+
+    /**
+     *
+     */
+    public function testFetchAllAfterEofReturnsEmptyArray()
+    {
+        $stream = fopen(__DIR__ . '/csv-files/simple.csv', 'rb');
+        $reader = new CsvReader($stream);
+        $reader->fetchAllAssoc();
+
+        $this->assertSame(array(), $reader->fetchAllAssoc());
+
+        fclose($stream);
+
+        $this->setExpectedException('Exception');
+        $reader->fetchAllAssoc();
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    private function convertToDataUri($string)
+    {
+        return 'data://text/plain;base64,' . base64_encode($string);
+    }
+}
