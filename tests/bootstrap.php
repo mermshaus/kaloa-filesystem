bf0482c5418f5a544e8c76ab60ff8f71e474e41f
Marc Ermshaus <marc@ermshaus.org>
2016-09-12 03:38:42 +0200

    Fix: Don't advise users to pipe from curl into PHP.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 
 if (
-    (!$loader = @include __DIR__.'/../../../autoload.php')
-    && (!$loader = @include __DIR__.'/../vendor/autoload.php')
+    (!$loader = @include __DIR__ . '/../../../autoload.php')
+    && (!$loader = @include __DIR__ . '/../vendor/autoload.php')
 ) {
-    die("You must set up the project dependencies, run the following commands:\n"
-        . "curl -s http://getcomposer.org/installer | php\n"
-        . "php composer.phar install\n");
+    die("You have to set up the project dependencies via Composer:\n"
+        . "\$ composer install\n");
 }
