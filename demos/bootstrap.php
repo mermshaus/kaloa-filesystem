e3d1ea6bdbce0dc7de7fc64eb9a7398dc78c5d60
Marc Ermshaus <marc@ermshaus.org>
2016-12-15 03:26:07 +0100

    Remove explicit error reporting settings (bad style).

--- demos/bootstrap.php
+++ demos/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(-1);
-ini_set('display_errors', 1);
-
 if ((!$loader = @include __DIR__.'/../../../autoload.php')
         && (!$loader = @include __DIR__.'/../vendor/autoload.php')
 ) {
